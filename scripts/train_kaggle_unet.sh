428c176ccc8f14b481a8cdfb2db680b113ab9136
Tri.nq <tri.nq210860@sis.hust.edu.vn>
2023-11-17 16:16:38 +0700

    Kaggle congig: epoch-> 200

--- scripts/train_kaggle_unet.sh
+++ scripts/train_kaggle_unet.sh
@@ -1,12 +1,12 @@
 python PolypSegment/main.py \
     --model Unet \
-    --checkpoint-path ./checkpoints/unet_model.pth \
-    --pretrained-path ./saved_models/ \
+    --checkpoint-path /kaggle/working/PolypSegment/checkpoints/unet_model.pth \
+    --pretrained-path /kaggle/working/PolypSegment/saved_models/ \
     --infer-path /kaggle/working/PolypSegment/predicted_masks/ \
     --images-path /kaggle/input/bkai-igh-neopolyp/train/train/ \
     --masks-path /kaggle/input/bkai-igh-neopolyp/train_gt/train_gt/ \
     --test-path /kaggle/input/bkai-igh-neopolyp/test/test/ \
-    --num-epochs 50 \
+    --num-epochs 200 \
     --lr 2e-4 \
     --batch-size 4 \
     --display-step 10 \
