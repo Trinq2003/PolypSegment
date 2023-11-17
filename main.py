307a6183ca8b7f4c4d424160283def9eea3128c1
Tri.nq <tri.nq210860@sis.hust.edu.vn>
2023-11-17 22:34:11 +0700

    Change input transform

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ transform = transforms.Compose([
     transforms.RandomVerticalFlip(),
     transforms.GaussianBlur(3),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
 unet_dataset = UNetDataClass(images_path, masks_path, transform)
--- scripts/train_kaggle_res_unet_pp.sh
+++ scripts/train_kaggle_res_unet_pp.sh
@@ -6,7 +6,7 @@ python PolypSegment/main.py \
     --images-path /kaggle/input/bkai-igh-neopolyp/train/train/ \
     --masks-path /kaggle/input/bkai-igh-neopolyp/train_gt/train_gt/ \
     --test-path /kaggle/input/bkai-igh-neopolyp/test/test/ \
-    --num-epochs 50 \
+    --num-epochs 25 \
     --lr 2e-4 \
     --batch-size 4 \
     --display-step 10 \
