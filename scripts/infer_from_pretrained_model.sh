51be77aba95d152be18c2d37b99a2a24baaa064e
Tri.nq <tri.nq210860@sis.hust.edu.vn>
2023-11-18 15:55:15 +0700

    Change kaggle epoch

--- scripts/infer_from_pretrained_model.sh
+++
@@ -1,8 +0,0 @@
-python infer.py \
-    --model Unet \
-    --optimizer Adam \
-    --checkpoint-path ./checkpoints/unet_model.pth \
-    --pretrained-path ./saved_models/ \
-    --infer-path ./predicted_masks/ \
-    --lr 2e-4 \
-    --batch-size 4 
